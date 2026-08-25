bfa863f8200b0a5b79efb81508325961304df952
ehmedovamesume123-dotcom <ehmedovamesume123@gmail.com>
2026-08-25 15:29:09 +0400

    Create gun_13.py

---
+++ gun_13.py
@@ -0,0 +1,7 @@
+soz = "Mexanika mühəndisi"
+
+print(soz[0])      # ilk simvol
+print(soz[-1])     # son simvol
+print(soz[0:5])    # 0-cı indeksdən 5-ci indeksə qədər
+print(soz[5:])     # 5-ci indeksdən sona qədər
+print(soz[::-1])   # string-in tərs sırası
