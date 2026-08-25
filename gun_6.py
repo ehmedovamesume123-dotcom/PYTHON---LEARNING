8778616ef614d56ad8073b252691839f25a014c6
ehmedovamesume123-dotcom <ehmedovamesume123@gmail.com>
2026-08-25 13:31:40 +0400

    Create gun_6.py

---
+++ gun_6.py
@@ -0,0 +1,10 @@
+a = 25
+b = 3
+
+print("Toplama:", a + b)
+print("Çıxma:", a - b)
+print("Vurma:", a * b)
+print("Bölmə:", a / b)
+print("Tam bölmə:", a // b)
+print("Qalıq (mod):", a % b)
+print("Qüvvətə yüksəltmə:", a ** b)
