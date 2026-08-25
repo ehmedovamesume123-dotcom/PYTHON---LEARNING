3ee5141d5ef5c063aa9b5b2710040bb2597a1042
ehmedovamesume123-dotcom <ehmedovamesume123@gmail.com>
2026-08-25 13:18:14 +0400

    Create gun_4.py

---
+++ gun_4.py
@@ -0,0 +1,15 @@
+# 1. Mətni tam ədədə çevirmək
+a_metn = "25"
+a_eded = int(a_metn)
+print(a_eded)
+
+# 2. Onluq ədədi tam ədədə çevirmək
+A = 34.4
+a_tam = int(A)
+print(a_tam)
+
+# 3. Tam ədədi mətnə çevirmək
+a = 125
+a_metn = str(a)
+print(a_metn)
+
