5478b7bc2cd8912836fb8a47043da60cb2462404
ehmedovamesume123-dotcom <ehmedovamesume123@gmail.com>
2026-08-25 13:38:36 +0400

    Create gun_8.py

---
+++ gun_8.py
@@ -0,0 +1,6 @@
+ball = 75
+kecdi = True
+
+print(ball > 50 and kecdi)  # hər iki şərt doğrudursa True verir
+print(ball > 90 or kecdi)   # ən azı biri doğrudursa True verir
+print(not kecdi)            # şərtin əksini qaytarır (True-dursa False edir)
