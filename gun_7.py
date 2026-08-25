fc9beae8ca4fafc0a329855d2556d05dd280cfe0
ehmedovamesume123-dotcom <ehmedovamesume123@gmail.com>
2026-08-25 13:34:29 +0400

    Create gun_7.py

---
+++ gun_7.py
@@ -0,0 +1,9 @@
+a = 25
+b = 4
+
+print(a == b)  # bərabərdirmi
+print(a != b)  # bərabər deyilmi
+print(a > b)   # böyükdürmü
+print(a < b)   # kiçikdürmü
+print(a >= b)  # böyük və ya bərabərdirmi
+print(a <= b)  # kiçik və ya bərabərdirmi
